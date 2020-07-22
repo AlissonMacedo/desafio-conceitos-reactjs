8b8fc8fbd5775ab9176163fe3237fb32a86b8f39
Alisson Macedo <alissontecnico@gmail.com>
2020-07-22 20:16:48 -0300

    Desafio RocketSeat conceitos ReactJS - Adicionado funcao para listar repositorios - Adicionado funcao para adicionar novos repositorios - Adicionado funcao para remover repositorios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    loadRepository();
+  });
+
+  async function loadRepository() {
+    api.get("/repositories").then((response) => setRepositories(response.data));
+  }
+
   async function handleAddRepository() {
-    // TODO
+    const text = {
+      title: `Repositorio ${Date.now()}`,
+      url:
+        "https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-nodejs",
+      techs: ["React", "React-Native", "NodeJS"],
+    };
+    const { data } = await api.post("/repositories", text);
+
+    setRepositories([...repositories, data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`/repositories/${id}`);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map((repository) => (
+          <li>
+            {repository?.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository?.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
